62a97d05835dc5c9f6356358628bce42ee8e700c
surbhiti <surbhitiwari70@gmail.com>
2023-08-28 07:55:09 +0530

    my second push

--- src/test/java/simpleproject/Facebook.java
+++ src/test/java/simpleproject/Facebook.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 //first pull
+//second push
 public class Facebook {
 
 	public static void main(String[] args) {
