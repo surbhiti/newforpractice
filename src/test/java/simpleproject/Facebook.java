d3c1ad6fd4659d8216cec89aab7a641683ae1294
surbhiti <134397746+surbhiti@users.noreply.github.com>
2023-08-28 07:49:27 +0530

    Update Facebook.java

--- src/test/java/simpleproject/Facebook.java
+++ src/test/java/simpleproject/Facebook.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+//first pull
 public class Facebook {
 
 	public static void main(String[] args) {
